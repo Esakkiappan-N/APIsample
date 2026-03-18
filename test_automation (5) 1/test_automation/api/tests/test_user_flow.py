09f164a4b4c3eb2a127198110a893c4e32b7683f
Esakkiappan-N <esakki3124@gmail.com>
2026-03-18 18:14:03 +0530

    Update test_user_flow.py

--- test_automation (5) 1/test_automation/api/tests/test_user_flow.py
+++ test_automation (5) 1/test_automation/api/tests/test_user_flow.py
@@ -2,21 +2,18 @@ import pytest
 from API_actions.login_api import login_api, get_token
 from API_actions.user_api import get_user_profile
 from utils.csv_reader import read_csv
-
-
+ 
 @pytest.mark.parametrize("user", read_csv("testdata/api/test_data.csv"))
 def test_user_flow(user):
     login_response = login_api(user["username"], user["password"])
     print("Login Response:", login_response.text)
-
+    if user["username"] == "wronguser":
+        assert login_response.status_code == 400
+        return
     assert login_response.status_code == 200
-
     token = get_token(login_response)
     print("Token:", token)
-
     assert token is not None
-
     profile_response = get_user_profile(token)
     print("Profile Response:", profile_response.text)
-
     assert profile_response.status_code == 200
